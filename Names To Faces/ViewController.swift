c9dabf20cf3b1584907c7d9e57fe183c6250abeb
Forrest Parker <forrestwparker@gmail.com>
2018-05-02 10:08:49 -0700

    Completed Section 3: UICollectionView data sources

--- Names To Faces/ViewController.swift
+++ Names To Faces/ViewController.swift
@@ -12,11 +12,18 @@ class ViewController: UICollectionViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
